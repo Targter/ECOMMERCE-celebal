7ef4f3a30ec3998e1946ee4c25ffbb81a2e7aa55
Abhay Bansal <targeter001@gmail.com>
2025-07-13 20:31:28 +0530

    updated forntend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,9 @@ import store from "./reducers/store/storee.js";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
