ed8e34a0595d8a028a8be0b5e3b2d4c828678d84
Abhay Bansal <targeter001@gmail.com>
2025-07-14 18:12:46 +0530

    updated headerss

--- b2/app.js
+++ b2/app.js
@@ -19,8 +19,8 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
-    credentials: true, // Required for cookies
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -29,7 +29,7 @@ app.use(express.json({ limit: "50mb" })); // Increase JSON limit
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // Route Imports
 const product = require("./routes/productRoute");
-const user = require("./routes/userRoute"); 
+const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
